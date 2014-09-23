773c272f0f4a9ed9865dda9c1f84ba2f1cc23604
Dave <dave@Daves-MacBook-Pro.local>
2014-09-23 00:34:29 -0400

    Does it reject anything with .user.js?

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,17 @@
-javascript:(function() {var s=document.createElement('script');s.setAttribute('src','https://eurica.github.io/pducks/pd.user.js');document.getElementsByTagName('head')[0].appendChild(s);})();
+embedScript = function(src) {
+  s=document.createElement('script');
+  s.setAttribute('src',src);
+  document.getElementsByTagName('head')[0].appendChild(s); 
+}
+
+window.onload = function() {
+  console.log("Starting PDucks");
+
+  embedScript('https://eurica.github.io/pducks/all.js')
+  
+  if(window.location.href.indexOf("pagerduty.com/incidents/") > -1 ) {  
+    console.log("PDucks: Incident detail page");
+    (function() {var s=document.createElement('script');s.setAttribute('src','https://eurica.github.io/pducks/incident.js');document.getElementsByTagName('head')[0].appendChild(s);})();
+  }
+  console.log("Loaded PDucks");
+}
