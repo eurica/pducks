ea90d9dd7a352e12712f8d5f59ea9fe92347da65
Dave <dave@Daves-MacBook-Pro.local>
2014-09-23 00:32:48 -0400

    Make a bookmarkletable link

---
+++ bookmarklet.js
@@ -0,0 +1,1 @@
+javascript:(function() {var s=document.createElement('script');s.setAttribute('src','https://eurica.github.io/pducks/pd.user.js');document.getElementsByTagName('head')[0].appendChild(s);})();
